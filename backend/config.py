4a01069e9551408adf7dc18024649596c1f9ce66
Ashish Lal <ashish.lal@mathco.com>
2025-04-10 08:52:02 +0000

    config file updated to work from anywhere

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
 import torch
+import os
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-AUDIO_FOLDER = "local_uploads"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+AUDIO_FOLDER = os.path.join(BASE_DIR, "local_uploads")
